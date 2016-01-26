0a97a551ccaf9b7b2267535ae45c900cc030f48b
= <=>
2016-01-26 20:11:09 -0200

    desabilita site default do apache, infelizmente o puppet não esta reiniciando o apache

--- manifests/cp_vhost.pp
+++ manifests/cp_vhost.pp
@@ -1,11 +1,13 @@
 node default {
 	$server_name = 'unioeste_app_tcc'
 	$doc_root = '/var/www/unioeste_app_tcc/current/public'
-
+	$old_site = '000-default.conf'
+	
 	# create a simple config file for site in Apache for app Rails
 	class { 'ruby::passenger::vhost':
 		server_name		=> $server_name,
 		doc_root		=> $doc_root,
 		rails_env		=> 'production',
+		disable_site	=> $old_site,
 	}
 }
