58afa6d5098f4c6101c5a495622eedb3a1bec2a9
= <=>
2016-02-05 11:52:03 -0200

    's' atrapalhando o cp_vhost

--- manifests/cp_vhost.pp
+++ manifests/cp_vhost.pp
@@ -5,7 +5,7 @@ node default {
 	
 	# create a simple config file for site in Apache for app Rails
 	class { 'ruby::passenger::vhost':
-		server_name		=> $server_name,s
+		server_name		=> $server_name,
 		doc_root		=> $doc_root,
 		rails_env		=> 'production',
 		disable_site	=> $old_site,
